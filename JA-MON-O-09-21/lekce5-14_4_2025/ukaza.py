7fa2e238328066f3003c2cbaf37e33c8e1ba4a3f
elsnoxx <ficek.risa@gmail.com>
2025-04-14 18:18:19 +0200

    Create ukaza.py

---
+++ JA-MON-O-09-21/lekce5-14_4_2025/ukaza.py
@@ -0,0 +1,25 @@
+import requests
+
+# 1. Definujeme URL API koncového bodu
+url = "https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/"
+
+ico = input("Zadej ico hledaneho subjeku: ")
+
+url = url + ico
+# 2. Odešleme GET požadavek na server
+print("Odesílám požadavek na API...")
+response = requests.get(url)
+
+# 3. Zkontrolujeme, zda byl požadavek úspěšný (stavový kód 200)
+if response.status_code == 200:
+    # 4. Převedeme JSON odpověď na slovník Pythonu
+    data = response.json()
+    
+    # 5. Vypíšeme základní informace o ekonomickém subjektu
+    print("\nZÁKLADNÍ INFORMACE O SUBJEKTU:")
+    print(f"Název: {data['obchodniJmeno']}")
+    print(f"IČO: {data['ico']}")
+    print(f"Právní forma: {data['pravniForma']}")
+    print(f"Adresa: {data['sidlo']['textovaAdresa']}")
+else:
+    print(f"Chyba při komunikaci s API. Stavový kód: {response.status_code}")
